6928bec4298c5314744793220b7264cad694d95b
Harriet Dashnow <h.dashnow@gmail.com>
2017-05-04 17:58:45 +1000

    Write whole pipeline_config.groovy in install.sh, then have template as complete example for users to look at. Use bedtools sorted TRF annotation. fix some quotation marks

--- pipelines/config-examples/pipeline_config_template.groovy
+++ pipelines/config-examples/pipeline_config_template.groovy
@@ -9,4 +9,25 @@ PLATFORM='illumina'
 threads=8
 
 // For exome pipeline only ***Edit before running the exome pipeline***
-EXOME_TARGET="path/to/exome_target_regions.bed" 
+EXOME_TARGET=""
+
+// STRetch installation location
+STRETCH=""
+
+// Paths to tools used by the pipeline
+bpipe="$STRetch/tools/bin/bpipe"
+python="$STRetch/tools/bin/python"
+goleft="$STRetch/tools/bin/goleft"
+bedtools="$STRetch/tools/bin/bedtools"
+bwa="$STRetch/tools/bin/bwa"
+samtools="$STRetch/tools/bin/samtools"
+
+// Path to reference data
+refdir="$STRetch/reference-data"
+
+// Decoy reference assumed to have matching .genome file in the same directory
+REF="$refdir/hg19.STRdecoys.sorted.fasta"
+STR_BED="$refdir/hg19.simpleRepeat_period1-6_dedup.sorted.bed"
+DECOY_BED="$refdir/STRdecoys.sorted.bed"
+
+R="/usr/local/installed/R/3.3.2/lib64/R/bin/R"
