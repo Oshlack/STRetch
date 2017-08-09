47225186629e02b67a23849852aaf64ccf210238
Harriet Dashnow <h.dashnow@gmail.com>
2017-08-09 19:04:09 +1000

    R packages

---
+++ install-packages.R
@@ -0,0 +1,4 @@
+## Create the personal library if it doesn't exist. Ignore a warning if the directory already exists.
+dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)
+## Install packages
+install.packages(c('optparse','plyr','dplyr','tidyr','reshape2'))
