e2c89e0d9c1d49d38c3d76ce280aa777ff71444a
Harriet Dashnow <h.dashnow@gmail.com>
2017-08-30 13:56:47 +1000

    show warnings

--- install-packages.R
+++ install-packages.R
@@ -1,4 +1,4 @@
 ## Create the personal library if it doesn't exist. Ignore a warning if the directory already exists.
-dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)
+dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = TRUE, recursive = TRUE)
 ## Install packages
 install.packages(c('optparse','plyr','dplyr','tidyr','reshape2'))
