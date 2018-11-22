3b7c4a92d53ed9e43e2c1a5bbb9fbb121f059f37
Harriet Dashnow <h.dashnow@gmail.com>
2018-11-22 13:47:09 +1100

    first draft of STRetch exome from bam pipeline #42

---
+++ pipelines/STRetch_exome_bam_pipeline.groovy
@@ -0,0 +1,37 @@
+// Bpipe pipeline to detect pathogenic STR expansions from exome sequencing data
+// Takes a mapped bam as input and extracts relevant reads for remapping to STR decoys
+// Usage:
+// STRetch/tools/bin/bpipe run -p input_regions=all_STRs_in_genome.bed -p EXOME_TARGET=target_region.bed
+// STRetch/pipelines/STRetch_wgs_bam_pipeline.groovy sample1.bam sample2.bam
+// Note, EXOME_TARGET can also be set in pipeline_config.groovy
+
+// Load system configuration and other settings
+load 'pipeline_config.groovy'
+
+// Load Bpipe pipeline stages
+load 'pipeline_stages.groovy'
+
+input_type='bam'
+
+inputs "$input_type" : "Please supply one or more $input_type files to process"
+
+bwa_parallelism = 1
+
+shards = 1..bwa_parallelism
+
+init_shard = {
+    branch.shard = branch.name
+}
+
+run {
+    "%.${input_type}" * [
+        set_sample_info +
+        median_cov_region +
+        shards * [
+            init_shard + align_bwa_bam + index_bam
+        ] + merge_bams +
+        STR_coverage +
+        STR_locus_counts
+    ] +
+    estimate_size
+}
